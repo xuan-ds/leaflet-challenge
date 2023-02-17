52e29cef7765fc4635d3c72a5f0e4ef7bb7c2936
xuan-ds <116041459+xuan-ds@users.noreply.github.com>
2023-02-16 23:50:47 -0600

    uploading final hw

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# leaflet-challenge
+# Leaflet-Challenge / Module 15 Challenge
+### Background
+#### The United States Geological Survey, or USGS for short, is responsible for providing scientific data about natural hazards, the health of our ecosystems and environment, and the impacts of climate and land-use change. Their scientists develop new methods and tools to supply timely, relevant, and useful information about the Earth and its processes.
+#### The USGS is interested in building a new set of tools that will allow them to visualize their earthquake data. They collect a massive amount of data from all over the world each day, but they lack a meaningful way of displaying it. This project is to develop a way to visualize USGS data that will allow them to better educate the public and other government organizations (and hopefully secure more funding) on issues facing our planet.
+--- 
+### The steps to create the Earthquake Visualization:
+1. Get the dataset:
+- The USGS provides earthquake data in a number of different formats, updated every 5 minutes. Visit [the USGS GeoJSON Feed Links](doc:https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php) to an external site. page and choose a dataset to visualize. 
+![Getting Started](SS1.png)
+
+2. Import and visualize the data by doing the following:
+ - Using Leaflet, create a map that plots all the earthquakes from the dataset based on their longitude and latitude.
+ - Include popups that provide additional information about the earthquake when its associated marker is clicked.
+ - Create a legend that will provide context for your map data.
+ ![Getting Started](SS2.png)
+ ---
+### References
+Dataset created by [the United States Geological Survey](doc:https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php).
---
+++ static/js/logic.js
@@ -0,0 +1,49 @@
+// Store JSON endpoint as datadataUrl.
+var dataUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL/
+d3.json(dataUrl).then(function (data) {
+    // Send the data.features object to the createFeatures function.
+    createFeatures(data.features);
+  });
+  
+  function createFeatures(earthquakeData) {
+  
+    // Define a function that we want to run once for each feature in the features array.
+    // Give each feature a popup that describes the place and time of the earthquake.
+    function onEachFeature(feature, layer) {
+      layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    }
+  
+    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+    // Run the onEachFeature function once for each piece of data in the array.
+    var earthquakes = L.geoJSON(earthquakeData, {
+      onEachFeature: onEachFeature
+    });
+  
+    // Send earthquakes layer to the createMap function/
+    createMap(earthquakes);
+  }
+  
+  function createMap(earthquakes) {
+  
+    // Create the base layers.
+    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+  
+    var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+  
+    // Create map.
+    var myMap = L.map("map", {
+    center: [
+    36.77, -119.42
+    ],
+     zoom: 6,
+     layers: [street, earthquakes]
+   });
+
+  }
+  
